e1e07a508c7d9f7ccddf1671e1a9f955e1c4d070
[treasure3210] <treasure3210@gmail.com>
2023-01-23 15:32:30 -0700

    Generics Practice using Class, Function and adding constraints to restrict return type

--- Generics.playground/Contents.swift
+++ Generics.playground/Contents.swift
@@ -1,7 +1,45 @@
 import Foundation
 
+/// Function
+
 func displayData<T>(data: T) {
     print(data)
 }
 
 displayData(data: "Hello World")
+
+/// Class
+
+class MyData<T> {
+    var data: T
+    init(data: T) {
+        self.data = data
+    }
+    
+    func getData() -> T {
+        return self.data
+    }
+}
+
+var myInt = MyData<Int>(data: 6)
+var myString = MyData<String>(data: "Launchpad by Vog")
+
+print("My Int: \(myInt.getData())")
+print("My String: \(myString.getData())")
+
+/// Constraints
+
+class MyData1<T: Numeric> {
+    var data: T
+    init(data: T) {
+        self.data = data
+    }
+    
+    func getData() -> T {
+        return self.data
+    }
+}
+
+var myInt2 = MyData1(data: 6)
+
+print("My Int2: \(myInt2.getData())")
